93f1623932a207c83e478baaa96166e4f8369595
k rahul pandit <147794873+rahulpandit-123@users.noreply.github.com>
2026-06-03 16:02:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your code here
+let dragged = null;
+let images = document.querySelectorAll(".image")
+for (let image of images) {
+
+    image.addEventListener("dragstart", (e) => {
+        dragged = e.target;
+    });
+
+    image.addEventListener("dragover", (e) => {
+        e.preventDefault();
+    });
+
+    image.addEventListener("drop", (e) => {
+        e.preventDefault();
+
+        let target = e.target;
+
+        let parent = target.parentNode;
+
+        let temp = document.createElement("div");
+
+        parent.replaceChild(temp, dragged);
+        parent.replaceChild(dragged, target);
+        parent.replaceChild(target, temp);
+    });
+}
